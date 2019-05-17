f06736a4cee6386bb3d2589a3889aecad69ca5af
LegendEffects <Evaninho94@gmail.com>
2019-05-17 18:43:39 +0100

    Added colour switcher (keybind e)

--- src/components/index.js
+++ src/components/index.js
@@ -6,9 +6,12 @@ module.exports = {
         'pixelGrid': Vue.component('pixelgrid', require('./pixelGrid.vue')),
         'palette': Vue.component('palette', require('./palette.vue')),
         'toolbox': Vue.component('toolbox', require('./toolbox.vue')),
+        
         'importpanel': Vue.component('importpanel', require('./panels/import.vue')),
         'exportpanel': Vue.component('exportpanel', require('./panels/export.vue')),
         'clearpanel': Vue.component('clearpanel', require('./panels/clear.vue')),
         'settingspanel': Vue.component('settingspanel', require('./panels/settings.vue')),
+
+        'colourswitcher': Vue.component('colourswitcher', require('./toolbox/colourSwitcher.vue')),
     }
 }
